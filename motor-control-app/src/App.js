77b1fc7f94a6136718b134f77274c7d65b52971c
iñaki jose echaide <inakiechaide457@gmail.com>
2025-08-07 10:18:16 -0300

    Corregidos errores de ESLint en App.js

--- motor-control-app/src/App.js
+++ motor-control-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { 
   Button, 
   Container, 
@@ -8,7 +8,7 @@ import {
   CircularProgress,
   Fade,
   Zoom,
-  useTheme,
+  // useTheme,
   ThemeProvider,
   createTheme,
   CssBaseline
